e8ed04a3c55a2bd78277f6b3959f4be3a4f75317
Rafhael <rafhaelhailar@yahoo.com>
2023-10-08 11:15:49 +0800

    Seiryou

--- src/Components/MouseIndicator.js
+++ src/Components/MouseIndicator.js
@@ -16,26 +16,6 @@ const MouseIndicator = () => {
             const projects_container = document.querySelector("#projects .projects-container");
             const projects = document.querySelectorAll("#projects .project");   
 
-/* 
-            for (let i = 0;i < projects.length;i++) {
-                let target = projects[i];
-
-                const image = target.querySelector("img");
-                let action = function(x,y) {
-                    const image_box = target.getBoundingClientRect();
-                    image.style.top =  y - image_box.y + 20 + "px";
-                    image.style.left =  x - image_box.x - image_box.width / 2 + "px";
-                    image.classList.remove("hide");
-                    target.style.zIndex = 99;
-                }
-                let unTarget = function() {
-                    image.classList.add("hide");
-                    target.style.zIndex = 1;
-                }
-        
-                addTarget(target,action,unTarget);
-            }
- */
             (function() {
                 let buttons = document.querySelectorAll("button");
                 let as = document.querySelectorAll("a");
@@ -74,6 +54,7 @@ const MouseIndicator = () => {
             const mouse_follower = document.querySelector(".mouse_indicator .outer_circle");
             const mouse_follower_box = mouse_follower.getBoundingClientRect();
             
+            /* Cursor */
             window.addEventListener("mousemove",function(event) {
                 const mouse_indicator_box = mouse_indicator.getBoundingClientRect();
                 mouse_indicator.style.transform = `translate(${event.pageX - mouse_indicator_box.width / 2}px,${event.pageY - mouse_indicator_box.height / 2}px)`;
@@ -88,7 +69,6 @@ const MouseIndicator = () => {
                     if (x > target_box.x && x < target_box.x + target_box.width && y > target_box.y && y < target_box.y + target_box.height && !(projects_container.style.zIndex == -1 && !target.classList.contains("prevProjLink"))) {
                         action && action(mouse_indicator_box.left,mouse_indicator_box.top,mouse_follower);
                         text += `scale(1.5)`;
-                        console.log("YAGO")
                     } else unTarget && unTarget();
                 }
                 
@@ -113,7 +93,7 @@ const MouseIndicator = () => {
                             document.querySelector("#projects .project-preview .project-target").style.transition = "opacity 1s";
                             setTimeout(() => {
                                 document.querySelector("#projects .project-preview .project-target").style.opacity = 1;
-                            })
+                            },1);
                         },1000);
                     }
                 }
